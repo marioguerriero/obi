a2c11f3271793059d6ddcab1f2a9481631d550e3
Mario Guerriero <mario.guerriero@deliveryhero.com>
2018-07-30 10:30:42 +0200

    update heartbeat message class name

--- heartbeat/hb_sender.py
+++ heartbeat/hb_sender.py
@@ -3,8 +3,12 @@ import socket
 from .hb_pb2 import *
 
 HOSTNAME = socket.gethostname()
-
 CLUSTER_NAME = '-'.join(HOSTNAME.split('-')[:-1])
+
+QUERY = 'jmx?qry=Hadoop:service=ResourceManager,name=QueueMetrics,q0=root,q1=default'
+QUERY_URL = '{}:8088/{}'.format(HOSTNAME, QUERY)
+
+RECEIVER_ADDRESS = ''
 
 
 def send_hb():
@@ -12,10 +16,12 @@ def send_hb():
 
 
 def compute_msg():
-    # Start building HB message
+    # Initialize HB message
     hb = Heartbeat()
     hb.cluster_name = CLUSTER_NAME
 
     # Collect metrics from Yarn master
+
+    # Finish building heartbeat message
 
     return hb
