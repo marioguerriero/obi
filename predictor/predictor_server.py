f681ef869ef24dca48c95eb96783e9fc2fcdc087
Mario Guerriero <mario.guerriero@deliveryhero.com>
2018-07-30 13:53:27 +0200

    handle errors in predictor server

--- predictor/predictor_server.py
+++ predictor/predictor_server.py
@@ -1,10 +1,44 @@
 from flask import Flask
+from flask import request
 
-from .config import APP_NAME
+import json
+
+from .config import APP_NAME, REQUIRED_FIELDS
 
 app = Flask(APP_NAME)
 
 
-@app.route("/")
+@app.route("/predict-duration")
 def hello():
+    # Parse request arguments
+    profile = request.args.get('profile')
+
+    # Handle missing profile errors
+    if profile is None or profile not in REQUIRED_FIELDS:
+        resp = app.response_class(
+            response=json.dumps({
+                'error': 'Malformed request. '
+                         'Profile query parameter missing or invalid.'
+            }),
+            status=400,
+            mimetype='application/json'
+        )
+        return resp
+
+    # Handle missing query fields errors
+    required_fields = REQUIRED_FIELDS[profile]
+    for f in required_fields:
+        val = request.args.get(f)
+        if val is None:
+            resp = app.response_class(
+                response=json.dumps({
+                    'error': 'Malformed request. '
+                             'Profile {}  requires "{}" field.'.format(profile,
+                                                                       f)
+                }),
+                status=400,
+                mimetype='application/json'
+            )
+            return resp
+
     return "Hello World!"
