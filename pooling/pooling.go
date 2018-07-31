0813a8ef3f625dc060426ac3698956168b5b12bc
Mario Guerriero <mario.guerriero@deliveryhero.com>
2018-07-31 11:08:52 +0200

    implement pooling main function

--- pooling/pooling.go
+++ pooling/pooling.go
@@ -1,1 +1,28 @@
-package pooling
+package main
+
+import (
+	"obi/platforms"
+	"obi/model"
+	"os"
+	"github.com/golang/glog"
+)
+
+func main() {
+	// Read project name
+	proj := "projects/" + os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if proj == "" {
+		glog.Error(" GOOGLE_CLOUD_PROJECT env not set")
+	}
+
+	// Create cluster object
+	cluster := platforms.NewDataprocCluster(&model.ClusterBase{
+		Name: "obi-test-cluster",
+		Nodes: 2,
+	}, proj, "europe-west3-b","europe-west3", 1, 0.3)
+
+	// Allocate cluster resources
+	cluster.AllocateResources()
+
+	// Schedule some jobs
+	cluster.SubmitJob()
+}
