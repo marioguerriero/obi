268492225dd4cb6c1a7d62ec36930f77f0798303
Mario Guerriero <mario.guerriero@deliveryhero.com>
2018-08-13 17:37:27 +0200

    add infrastructure creation capabilities to client

--- client/k8s.py
+++ client/k8s.py
@@ -1,3 +1,10 @@
+import os
+
+import yaml
+
+import base64
+from random import randint
+
 import kubernetes as k8s
 
 import grpc
@@ -12,6 +19,14 @@ import utils
 from generic_client import GenericClient
 
 
+class FieldMissingError(Exception):
+    """
+    This exception should be triggered when a mandatory field has been
+    omitted from a configuration file
+    """
+    pass
+
+
 class KubernetesClient(GenericClient):
     #############
     #  START: Abstract methods from GenericClient
@@ -22,8 +37,8 @@ class KubernetesClient(GenericClient):
         """
         # Create lookup maps for functions
         self._create_f = {
-            'job': self._submit_job,
-            'infrastructure': self._submit_infrastructure,
+            'job': self._create_job,
+            'infrastructure': self._create_infrastructure,
         }
         self._get_f = {
             'job': self._get_jobs,
@@ -82,7 +97,7 @@ class KubernetesClient(GenericClient):
     #  END: Abstract methods from GenericClient
     #############
 
-    def _submit_job(self, **kwargs):
+    def _create_job(self, **kwargs):
         """
         Submit a job to OBI according to the given request
         :param submit_job_request:
@@ -116,12 +131,56 @@ class KubernetesClient(GenericClient):
             # Submit job creation request
             stub.SubmitJob(req)
 
-    def _submit_infrastructure(self, **kwargs):
+    def _create_infrastructure(self, **kwargs):
         """
         Submit an infrastructure to OBI according to the given request
         :param submit_job_request:
         :return:
         """
+        # Read deployment YAML file
+        with open(kwargs['infrastructure_path'], 'r') as df:
+            deployment = yaml.load(df)
+
+        # Get name
+        name = deployment['name'] if 'name' in deployment \
+            else self._object_name_generator()
+
+        # Get namespace
+        namespace = deployment['namespace'] if 'namespace' in deployment \
+            else self._user_config['kubernetesNamespace']
+
+        # Check if the mandatory fields were specified
+        fields = ['serviceAccountPath', 'projectId', 'region', 'zone']
+        for f in fields:
+            if f not in deployment:
+                raise FieldMissingError('"{}" field is mandatory'.format(f))
+
+        # Generate secret
+        secret_name = self._create_secret_from_file(
+            namespace, deployment['serviceAccountPath'])
+
+        # Generate label to be used to match services to deployments
+        label = self._object_name_generator(
+            prefix='{}-deployment'.format(
+                self._user_config['kubernetesNamespace']))
+
+        # Create master service
+        self._create_master_service(
+            namespace, label)
+
+        # Create heartbeat service
+        heartbeat_host, heartbeat_port = self._create_heartbeat_service(
+            namespace, label)
+
+        # Create config map to be used in the deployment
+        config_map_name = self._create_config_map(
+            namespace,
+            heartbeatHost=heartbeat_host, heartbeatPort=heartbeat_port)
+
+        # Create deployment
+        self._create_infrastructure_deployment(
+            namespace, deployment['projectId'], secret_name, label,
+            config_map_name)
 
     def _delete_job(self, **kwargs):
         """
@@ -177,6 +236,293 @@ class KubernetesClient(GenericClient):
         Discover all the OBI available platform services
         :return: list of available services
         """
+
+    def _object_name_generator(self, prefix=None):
+        """
+        This method simply generates a random name for an OBI infrastructure.
+        It is called when the user does not specify a name while creating an
+        infrastructure.
+        :return:
+        """
+        if prefix is None:
+            prefix = self._user_config['masterHost']
+        return '{}-{}'.format(prefix, randint(1000, 9999))
+
+    def _create_secret_from_file(self, namespace, path):
+        """
+        This function create a namespaced secret from the given file and return
+        the secret name on k8s
+        :param namespace:
+        :param path:
+        :return:
+        """
+        # Open file
+        with open(path, 'r') as f:
+            secret_content = f.read()
+
+        # It was noticed that any newline character or '\n' sequence
+        # causes the secret not to be decoded properly. That's why
+        # I am avoiding those cases here
+        secret_content = secret_content.replace('\n', '').replace('\r', '') \
+            .replace('\\n', '').replace('\\r', '')
+
+        # Generate secret
+        secret_name = self._object_name_generator(
+            prefix='{}-secret'.format(
+                self._user_config['kubernetesNamespace']))
+        secret = k8s.client.V1Secret()
+        secret.data = {
+            secret_name: base64.b64encode(secret_content)
+        }
+
+        # Send secret creation request
+        try:
+            api_response = self._client.create_namespaced_secret(namespace,
+                                                                 secret,
+                                                                 pretty='true')
+            log.info(api_response)
+            return secret_name
+        except k8s.client.rest.ApiException as e:
+            log.error(
+                "Exception when calling CoreV1Api->create_namespaced_secret: "
+                "%s" % e)
+
+    def _create_infrastructure_deployment(self, namespace, project_id,
+                                          sa_secret, label, config_map_name):
+        """
+        This function is used to generate the deployment for the OBI
+        master for a certain infrastructure. This function returns the
+        name of the selectors to be used by services to address
+        the generated deployment
+        :return:
+        """
+        # Create Deployment Object
+        deployment = k8s.client.V1Deployment()
+
+        # Create metadata object
+        metadata = k8s.client.V1ObjectMeta()
+        metadata.namespace = namespace
+        deployment.metadata = namespace
+
+        # Create Spec object
+        deployment.spec = self._build_deployment_spec_object(label,
+                                                             project_id,
+                                                             sa_secret,
+                                                             config_map_name)
+
+        # Send deployment creation request
+        try:
+            api_response = self._client.create_namespaced_deployment(
+                namespace, deployment, pretty='true')
+            log.info(api_response)
+        except k8s.client.rest.ApiException as e:
+            log.error(
+                "Exception when calling CoreV1Api->create_namespaced_secret: "
+                "%s" % e)
+
+    def _build_deployment_spec_object(self, label, project_id,
+                                      sa_secret, config_map_name):
+        """
+        This function simply creates a spec object to attach to the
+        deployment of a new OBI master
+        :param label:
+        :param project_id:
+        :param sa_secret:
+        :return: the generated spec object
+        """
+        spec = k8s.client.V1DeploymentSpec()
+        spec.replicas = self._user_config['masterReplicas']
+
+        selector = k8s.client.V1LabelSelector()
+        selector.match_labels = {
+            self._user_config['defaultServiceSelectorName']: label
+        }
+        spec.selector = selector
+
+        template = k8s.client.V1PodTemplateSpec()
+        meta = k8s.client.V1ObjectMeta()
+        meta.labels = {
+            self._user_config['defaultServiceSelectorName']: label
+        }
+        template.metadata = meta
+
+        template_spec = k8s.client.V1PodSpec()
+        container = k8s.client.V1Container()
+        container.image = self._user_config['masterImage']
+
+        # Volume mount for secret
+        volume_mount_secret = k8s.client.V1VolumeMount()
+        volume_secret_mount_name = self._object_name_generator(
+            prefix='{}-volumne-mount'.format(
+                self._user_config['kubernetesNamespace']))
+        volume_mount_secret.name = volume_secret_mount_name
+        volume_mount_secret.mount_path = self._user_config['secretMountPath']
+
+        # Volume mount for config map
+        volume_mount_config_map = k8s.client.V1VolumeMount()
+        volume_mount_config_map_name = self._object_name_generator(
+            prefix='{}-volumne-mount'.format(
+                self._user_config['kubernetesNamespace']))
+        volume_mount_config_map.name = volume_mount_config_map_name
+        volume_mount_config_map.mount_path = \
+            self._user_config['configMountPath']
+
+        container.volume_mounts = [
+            volume_mount_secret, volume_mount_config_map
+        ]
+
+        # Environment variables
+        env1 = k8s.client.V1EnvVar()
+        env1.name = 'GOOGLE_CLOUD_PROJECT'
+        env1.value = project_id
+        env2 = k8s.client.V1EnvVar()
+        env2.name = 'GOOGLE_APPLICATION_CREDENTIALS'
+        env2.value = os.path.join(
+            self._user_config['secretMountPath'], sa_secret)
+        container.env = [
+            env1, env2
+        ]
+        template_spec.containers = [
+            container
+        ]
+
+        # Volume for SA secret
+        volume_secret = k8s.client.V1Volume()
+        volume_secret.name = volume_secret_mount_name
+        volume_secret.secret = k8s.client.V1SecretVolumeSource()
+        volume_secret.secret.secret_name = sa_secret
+
+        # Volume for config map
+        volume_config_map = k8s.client.V1Volume()
+        volume_config_map.name = volume_mount_config_map_name
+        volume_config_map.config_map = k8s.client.V1ConfigMapVolumeSource()
+        volume_config_map.config_map.name = config_map_name
+
+        template_spec.volumes = [
+            volume_secret, volume_config_map
+        ]
+        template.spec = template_spec
+
+        spec.template = template
+
+        return spec
+
+    def _create_master_service(self, namespace, label):
+        """
+        This function a OBI master service and returns its
+        public IP address and port
+        :return:
+        """
+        # Create service object
+        service = k8s.client.V1Service()
+
+        # Metadata object
+        metadata = k8s.client.V1ObjectMeta()
+        metadata.name = namespace
+        service.metadata = metadata
+
+        # Spec object
+        spec = k8s.client.V1ServiceSpec()
+        spec.type = 'LoadBalancer'
+        port = k8s.client.V1ServicePort()
+        port.port = self._user_config['defaultMasterPort']
+        port.protocol = 'TCP'
+        spec.ports = [
+            port
+        ]
+        spec.selector = {
+            self._user_config['defaultServiceSelectorName']: label
+        }
+
+        service.spec = spec
+
+        try:
+            api_response = self._client.create_namespaced_service(
+                namespace, service, pretty='true')
+            log.info(api_response)
+            return api_response.spec.load_balancer_ip, port.port
+        except k8s.client.ApiException as e:
+            log.error(
+                "Exception when calling CoreV1Api->create_namespaced_service: "
+                "%s\n" % e)
+            return None
+
+    def _create_heartbeat_service(self, namespace, label):
+        """
+        This function a OBI master service and returns its
+        public IP address and port
+        :return:
+        """
+        # Create service object
+        service = k8s.client.V1Service()
+
+        # Metadata object
+        metadata = k8s.client.V1ObjectMeta()
+        metadata.name = namespace
+        service.metadata = metadata
+
+        # Spec object
+        spec = k8s.client.V1ServiceSpec()
+        spec.type = 'LoadBalancer'
+        port = k8s.client.V1ServicePort()
+        port.port = self._user_config['defaultHeartbeatPort']
+        port.protocol = 'UDP'
+        spec.ports = [
+            port
+        ]
+        spec.selector = {
+            self._user_config['defaultServiceSelectorName']: label
+        }
+
+        service.spec = spec
+
+        try:
+            api_response = self._client.create_namespaced_service(
+                namespace, service, pretty='true')
+            log.info(api_response)
+            return api_response.spec.load_balancer_ip, port.port
+        except k8s.client.ApiException as e:
+            log.error(
+                "Exception when calling CoreV1Api->create_namespaced_service: "
+                "%s\n" % e)
+            return None
+
+    def _create_config_map(self, namespace, **kwargs):
+        """
+        This function is used to generate a k8s config map, to be passed
+        to a deployment so that the OBI master can use it to retrieve
+        its configuration details.
+        :param namespace:
+        :return: the name of the generated config map
+        """
+        # Generate a name for the config map
+        name = self._object_name_generator(
+            prefix='{}-config-map'.format(
+                self._user_config['kubernetesNamespace']))
+
+        # Generate config map content
+        cm_content = yaml.dump(**kwargs)
+
+        # Create config map object
+        config_map = k8s.client.V1ConfigMap()
+        config_map.data = {
+            self._user_config['defaultConfigMapName']: cm_content
+        }
+
+        metadata = k8s.client.V1ObjectMeta()
+        metadata.name = name
+        config_map.metadata = metadata
+
+        try:
+            api_response = self._client.create_namespaced_config_map(
+                namespace, config_map, pretty='true')
+            log.info(api_response)
+            return name
+        except k8s.client.ApiException as e:
+            log.error(
+                "Exception when calling "
+                "CoreV1Api->create_namespaced_config_map: %s\n" % e)
+            return None
     #############
     #  END: Generic utility functions
     #############
