986d9de08ba5ab4e3a08101a08492171f47ee5f2
Mario Guerriero <mario.guerriero@deliveryhero.com>
2018-08-30 11:24:09 +0200

    implement master-predictor communication, update master docker image dependency

--- master/master.go
+++ master/master.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
-	"obi/master/pooling"
-		"obi/master/heartbeat"
+	"fmt"
 	"github.com/sirupsen/logrus"
-	)
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"log"
+	"obi/master/heartbeat"
+	"obi/master/pooling"
+)
 
 // ObiMaster structure representing one master instance for OBI
 type ObiMaster struct {
 	Pooling *pooling.Pooling
 	HeartbeatReceiver *heartbeat.Receiver
+	PredictorClient *ObiPredictorClient
 }
 
 // ListInfrastructures RPC for listing the available infrastructure services
@@ -26,6 +31,16 @@ func (m *ObiMaster) SubmitJob(ctx context.Context,
 		jobRequest *SubmitJobRequest) (*EmptyResponse, error) {
 	logrus.WithField("path", jobRequest.Job.ExecutablePath).Info("Received job request")
 
+	// Generate predictions before submitting the job
+	resp, err := (*m.PredictorClient).RequestPrediction(
+		context.Background(), &PredictionRequest{
+			JobFilePath: jobRequest.Job.ExecutablePath,
+		}) // TODO: read metrics for executor cluster
+	if err != nil {
+		logrus.WithField("response", resp).Warning("Could not generate predictions")
+	}
+
+	// Send job execution request
 	switch jobRequest.Job.Type {
 	case Job_PYSPARK:
 		m.Pooling.SubmitPySparkJob("obi-test", jobRequest.Job.ExecutablePath)
@@ -44,10 +59,21 @@ func CreateMaster() (*ObiMaster) {
 	hb := heartbeat.New(pool, 60, 30)
 	hb.Start()
 
+	// Open connection to predictor server
+	serverAddr := fmt.Sprintf("%s:%s",
+		viper.GetString("predictorHost"),
+		viper.GetString("predictorPort"))
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure()) // TODO: encrypt communication
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	pClient := NewObiPredictorClient(conn)
+	logrus.WithField("address", serverAddr).Info("Connection")
 	// Create and return OBI master object
 	master := ObiMaster {
 		Pooling: p,
 		HeartbeatReceiver: hb,
+		PredictorClient: &pClient,
 	}
 	return &master
 }
