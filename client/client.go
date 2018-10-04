b2e061a892b6e016801d0b707c9434f0ebeb1932
Luca Lombardo <luca.lombardo@deliveryhero.com>
2018-10-04 10:13:35 +0200

    Get automatically LoadBalancer address

--- client/client.go
+++ client/client.go
@@ -12,8 +12,14 @@ import (
 	"cloud.google.com/go/storage"
 	"io"
 	"path"
-	uuid "github.com/satori/go.uuid"
+	"github.com/satori/go.uuid"
 	"fmt"
+	"golang.org/x/crypto/ssh/terminal"
+			"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"path/filepath"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 type obiCreds struct {
@@ -35,13 +41,18 @@ func (c obiCreds) RequireTransportSecurity() bool {
 
 func main() {
 	var jobRequestType JobSubmissionRequest_JobType
+	var username string
+	var kubeconfig *string
+
+	// parsing arguments
 	execPath := flag.StringP("path", "f", "", "a string")
-	infrastracture := flag.StringP("infrastructure", "i", "", "a string")
+	infrastructure := flag.StringP("infrastructure", "i", "", "a string")
 	jobType := flag.StringP("type", "t", "", "a string")
 	priority := flag.Int32P("priority", "p", 0, "an int")
 
 	flag.Parse()
 
+	// fill job request struct
 	jobArgs :=strings.Join(flag.Args(), " ")
 
 	if *jobType == "PySpark" {
@@ -73,22 +84,64 @@ func main() {
 
 	jobRequest := JobSubmissionRequest{
 		ExecutablePath:       *execPath,
-		Infrastructure:       *infrastracture,
+		Infrastructure:       *infrastructure,
 		Type:                 jobRequestType,
 		JobArgs:              jobArgs,
 		Priority:             *priority,
 	}
-	submitJob(jobRequest)
+
+	// ask for credentials
+	fmt.Println("Username: ")
+	fmt.Scanf("%s\n", &username)
+	fmt.Println("Password: ")
+	password, err := terminal.ReadPassword(0)
+	if err != nil {
+		log.Fatal("Something went wrong. Sorry.")
+	}
+
+	creds := obiCreds {
+		username,
+		string(password),
+	}
+
+	// load kubeconfig
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// get service to contact obi master endpoint
+	deployment, err := clientset.AppsV1().Deployments("obi").Get(*infrastructure, metav1.GetOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	serviceName := deployment.Annotations["master-service-name"]
+	service, err := clientset.CoreV1().Services("obi").Get(serviceName, metav1.GetOptions{})
+	if ingresses := service.Status.LoadBalancer.Ingress;  len(ingresses) == 0 {
+		log.Fatal("Service not reachable.")
+	}
+	serviceAddress := service.Status.LoadBalancer.Ingress[0].IP
+	submitJob(jobRequest, creds, serviceAddress)
 }
 
-func submitJob(request JobSubmissionRequest) {
-	creds := obiCreds {
-		"luca",
-		"ciao",
-	}
+func submitJob(request JobSubmissionRequest, creds obiCreds, address string) {
 	credentials := credentials.NewTLS( &tls.Config{ InsecureSkipVerify: true } )
 	conn, err := grpc.Dial(
-		"35.242.194.12:8081",
+		address + ":8081",
 		grpc.WithTransportCredentials(credentials),
 		grpc.WithPerRPCCredentials(creds),
 	)
@@ -108,4 +161,11 @@ func submitJob(request JobSubmissionRequest) {
 	}
 	fmt.Println("The job has been submitted correctly.")
 	fmt.Printf("The JobID is %d\n", resp.JobID)
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
