cac218e76da7cc29c13c5f145dff87f63ab92cbb
Mario Guerriero <mario.guerriero@deliveryhero.com>
2018-11-22 17:38:16 +0100

    [web] implement jwt based token authentication

--- web/backend/api.js
+++ web/backend/api.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+const secret = require('./secret');
+const jwt = require('jsonwebtoken');
+const jwt_middleware = require('express-jwt');
+
+const auth_verifier = jwt_middleware({
+    secret: secret
+});
+
 const query = require('./db');
 
 // Define API router
@@ -7,34 +15,36 @@ const router = express.Router();
 
 // Cluster data routes
 
-router.get('/clusters', function(req, res) {
-    // Filter based on cluster status
-    let cluster_status = req.query.status;
-    if(cluster_status !== null) {
-      cluster_status = 'running'
-    }
+router.get('/clusters', auth_verifier, function(req, res) {
+    // Check for any possible filter
+    let cluster_status = req.query.status ? req.query.status : '%';
+    let cluster_name = req.query.name ? req.query.name : '%';
 
     // Execute query
 });
 
-router.get('/cluster/:id', function(req, res) {
+router.get('/cluster/:id', auth_verifier, function(req, res) {
 
 });
 
 // Jobs data routes
 
-router.get('/jobs', function(req, res) {
+router.get('/jobs', auth_verifier, function(req, res) {
 
 });
 
-router.get('/job/:id', function(req, res) {
+router.get('/job/:id', auth_verifier, function(req, res) {
 
 });
 
 // Authentication routes
 
 router.post('/login', function(req, res) {
+    // Check that username and password match the database
 
+    // Generate and send JWT token
+    const token = jwt.sign({} /* payload */, secret);
+    res.send(token);
 });
 
 // Export API router
